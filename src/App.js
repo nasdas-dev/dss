25324cac7c790d21765b98095bbbfb1361f08ed7
Dario Safa <dario.akhavansafa@uzh.ch>
2022-02-09 03:06:47 +0100

    update

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
           </div>
         </div>
         <div className="flex-container">
-          <div className="hero-content rounded-lg flex-1  flex-wrap w-5/6">
+          <div className="hero-content rounded-lg flex-1  flex-wrap w-5/6 shadow-sm backdrop-blur-md">
             <div className="card card-body bg-slate-50 backdrop-blur-sm">
               <h1 className="card-title text-3xl inset-1 font-bold ml-10 mr-10 p-4">
                 <span className="text-5xl font-serif font-black text-primary">
@@ -92,9 +92,12 @@ function App() {
                 at lorem arcu. Duis varius arcu nec bibendum iaculis. Mauris
                 vitae convallis.
               </p>
+              <button className="m-10 p-4 btn btn-secondary text-white border-0 shadow-2xl font-bold tracking-tight font-sans w-2/6">
+                Learn more
+              </button>{" "}
             </div>
           </div>
-          <div className="hero-content rounded-lg flex-1  flex-wrap w-5/6">
+          <div className="hero-content rounded-lg flex-1  flex-wrap w-5/6 shadow-sm">
             <div className="card card-body bg-slate-50 backdrop-blur-sm">
               <h1 className="card-title text-3xl inset-1 font-bold ml-10 mr-10 p-4">
                 <span className="text-5xl font-serif font-black text-secondary">
@@ -110,6 +113,9 @@ function App() {
                 porta ligula varius. Etiam venenatis euismod nisl sed porttitor.
                 Maecenas maximus
               </p>
+              <button className="m-10 p-4 btn btn-secondary text-white border-0 shadow-2xl font-bold tracking-tight font-sans w-2/6">
+                Learn more
+              </button>{" "}
             </div>
           </div>
         </div>
